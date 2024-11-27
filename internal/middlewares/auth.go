9d404ffb9af539e60f1e6820639229a47ced8d90
MrCrowNFT <mrcrownft@gmail.com>
2024-11-27 11:46:44 -0300

    Authenticarion Logic

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -5,20 +5,26 @@ import (
 	"context"
 	"net/http"
 	"strings"
+	"time"
 	"errors"
 	"github.com/golang-jwt/jwt/v5"
 )
+
+//todo need to use the .env for this
+var jwtKey = []byte("your_secret_key")
 
 // Claims structure
 type Claims struct {
 	UserID int    `json:"user_id"`
 	Role   string `json:"role"`
+	// This adds standard fields like issuer, expirationtime and audience 
 	jwt.RegisteredClaims
 }
 
+// Secure endpoint
 func AuthMiddleware(next http.Handler)(http.Handler){
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		// Extract tpken from authorization header
+		// Extract token from authorization header
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer "){
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
@@ -45,6 +51,31 @@ func AuthMiddleware(next http.Handler)(http.Handler){
 	})
 }
 
-func VerifyJWT(tokenString string)(*Claims, error){
+func GenerateJWT(userID int, role string)(string, error){
+	// Sets the token’s validity to 24 hours.
+	expirationTime := time.Now().Add(24* time.Hour)
+	claims := &Claims{
+		UserID: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			// Creates a token using the HS256 signing method and the claims
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Signs the token with the secret key, converting it into a string for transmission.
+	return token.SignedString(jwtKey)
+}
 
+func VerifyJWT(tokenString string)(*Claims, error){
+	claims := &Claims{}
+	//Decode the JWT and checks the signature against jwtKey
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error){
+		// Supplies the secret key for signature verification.
+		return jwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+	return claims, nil
 }
